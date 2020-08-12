ae97dc7586bd850fa63465ccc859cc6691fd7633
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-08-12 08:44:36 -0400

    added routes on app,js

--- WriterTyper/client/src/App.js
+++ WriterTyper/client/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import Main from './screens/Main/Main'
-
+import Upload from './screens/Upload/Upload'
+import Read from './screens/Read/Read'
+import Type from './screens/Type/Type'
 import './App.css';
 
 function App() {
   return (
     <>
-      <Main />
+      <Route path='/' exact>
+        <Main />
+      </Route>
+      <Route path='/upload'>
+        <Upload />
+      </Route>
+      <Route path='/read' exact>
+        <Read />
+      </Route>
+      <Route path='/type' exact>
+        <Type />
+      </Route>
     </>
   );
 }
--- WriterTyper/client/src/screens/Read/Read.js
+++ WriterTyper/client/src/screens/Read/Read.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Layout from '../../components/SHARED/Layout/Layout'
 function Read(props) {
   return (
     <Layout>
