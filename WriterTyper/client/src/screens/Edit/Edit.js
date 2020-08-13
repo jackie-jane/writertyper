8e769dff85849220872102f8b6e2a5ce2d76b8c1
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-08-13 14:43:21 -0400

    added textarea handlechange, wordcount, and charactercount

--- WriterTyper/client/src/screens/Edit/Edit.js
+++ WriterTyper/client/src/screens/Edit/Edit.js
@@ -6,28 +6,55 @@ import './Edit.css'
 class Edit extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      text: [],
+      wordCount: 0,
+      characterCount: 0,
+    }
 
   }
- 
   async componentDidMount() {
-    const authorId = this.props.match.authorId
-    const textId = this.props.match.textId
-    console.log(this.props)
-    console.log(authorId)
-    console.log(textId)
+    const authorId = this.props.match.params.authorId
+    const textId = this.props.match.params.textId
     const textToEdit = await readOneByAuthor(authorId, textId)
-    console.log(textToEdit)
+    this.setState({
+      text: textToEdit
+    })
+  }
+  wordCount(str) {
+    textArr = str.split(' ')
+    this.setState({
+      wordCount: textArr.length
+    })
+  }
+  characterCount(str) {
+    textArr = str.split('')
+    this.setState({
+      characterCount: textArr.length
+    })
+  }
+  handleChange = (event) => {
+    const newText = event.target
+    this.setState({
+      text: {
+        content: newText
+      }
+    })
+    wordCount(newText)
+    characterCount(newText)
   }
   render() {
+    const text = this.state.text
     return (
       <>
+        <textarea
+          defaultValue={text.content}
+          onChange={this.handleChange}
+        >
+        </textarea>
       </>
     );
   }
 }
-
-Edit.propTypes = {
-
-};
 
 export default withRouter(Edit);
