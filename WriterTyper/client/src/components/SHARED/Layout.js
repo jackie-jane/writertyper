e9c34c29fe5d385ca518316da16e45c98fcc601f
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-08-15 12:58:28 -0400

    rearranged componenets-SHARE

--- WriterTyper/client/src/components/SHARED/Layout.js
+++ WriterTyper/client/src/components/SHARED/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Main from './Main/Main'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import Main from '../Main/Main'
 
 function Layout(props) {
   return (
