4ef723186ed27db8f341c9a2899451cf9d96ce42
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-08-16 23:23:42 -0400

    began to create typing function

--- WriterTyper/client/src/screens/Type/Type.js
+++ WriterTyper/client/src/screens/Type/Type.js
@@ -1,23 +1,55 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+import { readOneByAuthor } from '../../services/Crud'
+import { withRouter } from 'react-router-dom'
 class Type extends Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      text: [],
+      upcoming: [],
+      completed: [],
+      currentChar: '',
+    }
   }
 
-  componentDidMount() {
+  async componentDidMount(z) {
+    const authorId = this.props.match.params.authorId
+    const textId = this.props.match.params.textId
+    const text = await readOneByAuthor(authorId, textId)
+    const str = text.content
+    const strArr = str.split('')
+    const firstLetter = strArr.shift()
+    // const upcomingArr = strArr.slice(0, 1)
+    this.setState({
+      text: str,
+      currentChar: firstLetter,
+      upcoming: strArr
+    })
+  }
 
+  handleType = (e) => {
+    const userInput = e.target.value
+    const char = this.state.currentChar
+    if (userInput === char) {
+      console.log('correct')
+    }
   }
 
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <textarea
+          value={this.state.currentChar}
+          disabled='disabled'
+        >
+        </textarea>
+        <textarea
+          onKeyUp={this.handleType}
+        >
+        </textarea>
+      </>
     );
   }
 }
 
-export default Type;
+export default withRouter(Type);
